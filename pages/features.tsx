cea6609f020d036b36e0e87b03ea8866f27bbd67
Jack <jack282323@gmail.com>
2022-09-10 11:55:04 +0200

    Update features.tsx

--- pages/features.tsx
+++ pages/features.tsx
@@ -65,10 +65,10 @@ const FEATURES = [
 
 export default function FeaturesPage() {
   return (
-    <Page title="Features" description="Elit aute do nisi Lorem id ea culpa sint duis eu tempor dolore elit.">
+    <Page title="Our Promise" description="We deliver high quality leads and results-driven email campaigns">
       <Wrapper>
         <SectionTitle>Check out this quick introduction</SectionTitle>
-        <YoutubeVideo url="https://www.youtube.com/watch?v=BggrpKfqh1c" />
+        We started
         <CustomAutofitGrid>
           {FEATURES.map((singleFeature, idx) => (
             <BasicCard key={singleFeature.title} {...singleFeature} />
